d36456edacf7d53ebc8053c86fae57c859a98280
aravinthraja <aravnth.raja@a-cti.com>
2017-02-07 22:20:51 +0530

    karma test

--- karma.config.js
+++ karma.config.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
 
 
     // list of files / patterns to load in the browser
-    files: ['js/src/real.js','js/test/real.js'],
+    files: ['js/src/*.js','js/test/*.js'],
 
 
     // list of files to exclude
